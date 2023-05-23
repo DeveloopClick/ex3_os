5131fb31fb232d2d6b0122bbc7c41d5986fb5b2f
DeveloopClick <develoop.click@gmail.com>
2023-05-23 19:22:26 +0300

    pushy

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -108,11 +108,12 @@ void emit3 (K3 *key, V3 *value, void *context)
   }
 }
 
-bool compare_k2(std::pair<K2 *, V2 *> *pair1,std::pair<K2 *, V2 *> *pair2)
+bool compare_k2(std::pair<K2 *, V2 *> &pair1,std::pair<K2 *, V2 *> &pair2)
 //check types
 {
-  return pair1->first > pair2->first;
+  return pair1.first > pair2.first;
 }
+
 void *run_job (WrappedContext *wrapped_context)
 {
 
@@ -155,15 +156,8 @@ void *run_job (WrappedContext *wrapped_context)
   {
     std::sort (context->thread_intermediate_vecs[thread_ind].begin (),
                context->thread_intermediate_vecs[thread_ind].end (), compare_k2);
-  }
 
-//    // List all unique keys (will be used for shuffle)
-//    std::transform (intermedVecs[i].begin (), intermedVecs[i].end (), back_inserter (this->uniqueK2Vecs[i]), [] (IntermediatePair &pair)
-//    { return pair.first; });
-//    IntermediateUniqueKeysVec::iterator it;
-//    it = std::unique (this->uniqueK2Vecs[i].begin (), this->uniqueK2Vecs[i].end (), K2equals);
-//    this->uniqueK2Vecs[i].resize ((unsigned long) std::distance (this->uniqueK2Vecs[i].begin (), it));
-//  }
+  }
 
   /************************************************
    *                  BARRIER                     *
@@ -187,79 +181,43 @@ void *run_job (WrappedContext *wrapped_context)
     // lock for the rest of the threads
     context->shuffleState = ShuffleState::IN_SHUFFLE;
 
-    // Collect all unique keys from all intermediate unique keys vectors
-    IntermediateUniqueKeysVec uniKeys; // Example: uniqueK2Vecs = {[1,2,3], [2,3], [1,3]}
-    for (int i = 0; i < context->numOfIntermediatesVecs; i++)
+  // MAKE SO ONLY ONE CONTINUES
+  while(!thread_intermediate_vecs.empty())
+  {
+    int max_ind = 0;
+    auto vec_of_max_pair = thread_intermediate_vecs[0];
+    for (int i = 0; i < thread_intermediate_vecs.length(); ++i)
     {
-      std::copy (context->uniqueK2Vecs[i].begin (), context->uniqueK2Vecs[i].end (), back_inserter (uniKeys));   // 10 20 30 20 10 0  0  0  0
-    }
-    // Unify into single vector of ordered unique keys
-    std::sort (uniKeys.begin (), uniKeys.end (), K2lessthan);
-    IntermediateUniqueKeysVec::iterator it;
-    it = std::unique (uniKeys.begin (), uniKeys.end (), K2equals);   // 10 20 30 20 10 ?  ?  ?  ?
-    uniKeys.resize ((unsigned long) std::distance (uniKeys.begin (), it)); // 10 20 30 20 10
-    context->uniqueK2Size = uniKeys.size ();
-
-    /************************************************
-    *                  PRODUCE TASKS                *
-    ************************************************/
-    // Go over ordered unique keys, foreach pop all pairs with this key from all vectors and launch reducer
-    while (!uniKeys.empty ())
-    {
-      // Get current key and extract all its pairs from all vectors
-      K2 *currKey = uniKeys.back ();
-      uniKeys.pop_back ();
-      auto keySpecificVec = IntermediateVec (); // TODO: Free at the end of reducer's procedure
-      // Go over all intermediate vectors
-      for (int j = 0; j < context->numOfIntermediatesVecs; j++)
+      auto vec = thread_intermediate_vecs[i];
+      if (vec.back() != NULL && vec_of_max_pair.back().first() < vec.back().first())
       {
-        // Extract all pairs with current key (if has any)
-        while ((!context->intermedVecs[j].empty ()) &&
-               K2equals (context->intermedVecs[j].back ().first, currKey))
+        vec_of_max_pair = vec;
+        max_ind = i;
+      }
+    }
+
+    IntermediateVec s_vec = IntermediateVec();  // TODO: free
+    vec.push_back(vec_of_max_pair.back());
+    vec_of_max_pair.pop_back();
+    if(vec_of_max_pair.empty())
+    {
+      //remove from thread_intermediate_vecs the ind of max_ind
+    }
+
+    for (int i = 0; i < thread_intermediate_vecs.length(); ++i)
+    {
+      auto vec = thread_intermediate_vecs[i];
+      if (vec.back() != NULL && s_vec.back().first() == vec.back().first())
+      {
+        s_vec.push_back(vec.back());
+        vec.pop_back();
+        if(vec.empty())
         {
-          keySpecificVec.push_back (context->intermedVecs[j].back ());
-          context->intermedVecs[j].pop_back ();
+          //remove from thread_intermediate_vecs the current ind
         }
-      } // All pairs with current key were processed into keySpecificVec - ready to reduce!
-
-      if (sem_wait (&context->taskQueueSem) != ErrorCode::SUCCESS)
-      {
-        fprintf (stderr, "Error: SHUFFLER Mutex (queue mutex) lock failure in waiting thread.\n");
-        exit (1);
-      }
-
-      context->readyQueue.push_back (keySpecificVec);
-
-      if (sem_post (&context->taskQueueSem) != ErrorCode::SUCCESS)
-      {
-        fprintf (stderr, "Error: SHUFFLER Mutex (queue mutex) UNlock failure in waiting thread.\n");
-        exit (1);
-      }
-
-      if (sem_post (&context->taskCountSem) != ErrorCode::SUCCESS)
-      {
-        fprintf (stderr, "Error: SHUFFLER Failed to post semaphore in shuffle stage.\n");
-        exit (1);
       }
     }
-    context->shuffleState = ShuffleState::DONE_SHUFFLING;
-
-    //special magic loop to release all remaining threads stuck on sem for task counter:
-    for (int i = 0; i < context->numOfIntermediatesVecs; i++)
-    {
-
-      if (sem_post (&context->taskCountSem) != ErrorCode::SUCCESS)
-      {
-        fprintf (stderr, "Error: SHUFFLER Failed to post semaphore in release loop\n");
-        exit (1);
-      }
-      if (sem_post (&context->taskQueueSem) != ErrorCode::SUCCESS)
-      {
-        fprintf (stderr, "Error: SHUFFLER Failed to post semaphore in release loop\n");
-        exit (1);
-      }
-
-    }
+    our_queue.push_back(s_vec);
   }
 
   /************************************************
